c8911678d771bcd8d7d8c75fb576695718e28b37
Fadil Karajic <fadil.karajic@gmail.com>
2019-08-04 00:19:40 -0500

    Add project files.

---
+++ DotNETMovies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNETMovies.MvcApplication" Language="C#" %>
---
+++ DotNETMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNETMovies.Startup))]
+namespace DotNETMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
