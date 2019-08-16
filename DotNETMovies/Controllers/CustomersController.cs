080853678c9da41b92325f11e7174ae58db4a7b7
Fadil Karajic <fadil.karajic@gmail.com>
2019-08-15 20:31:18 -0500

    Working on movie views

--- DotNETMovies/Controllers/CustomersController.cs
+++ DotNETMovies/Controllers/CustomersController.cs
@@ -2,21 +2,37 @@
 using System.Linq;
 using System.Web.Mvc;
 using DotNETMovies.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        //initialize applicationdbcontext to be able to use database
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //eager loading
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -24,13 +40,6 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Fadil1" },
-                new Customer { Id = 2, Name = "Fadil2" }
-            };
-        }
+       
     }
 }
--- DotNETMovies/Models/Genre.cs
+++ DotNETMovies/Models/Genre.cs
@@ -6,14 +6,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DotNETMovies.Models
 {
-    public class MembershipType
+    public class Genre
     {
         public byte Id { get; set; }
-        public short SignUpFee { get; set; }
-        public byte DurationInMonths { get; set; }
-        public byte DiscountRate { get; set; }
-
         [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
